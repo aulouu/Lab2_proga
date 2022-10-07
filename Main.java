6013dadcbc03dedb0f1f2b54ada1f54f40427490
Alina <113633949+aulouu@users.noreply.github.com>
2022-10-07 18:44:43 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -10,8 +10,8 @@ public class Main {
         Pokemon p2 = new Lileep("Лилип", 2);
         Pokemon p3 = new Cradily("Крадили", 1);
         Pokemon p4 = new Poliwag("Поливаг", 3);
-        Pokemon p5 = new Poliwhirl("Поливирл", 1);
-        Pokemon p6 = new Poliwrath("Поливрат", 2);
+        Pokemon p5 = new Poliwhirl("Поливирл", 2);
+        Pokemon p6 = new Poliwrath("Поливрат", 3);
 
         b.addAlly(p1);
         b.addAlly(p2);
